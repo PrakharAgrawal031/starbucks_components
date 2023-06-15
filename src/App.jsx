cdeb2f5ce7274365ecdba1e8f3154988b92b78d4
PrakharAgrawal031 <agrawalprakhar031@gmail.com>
2023-06-15 20:56:19 +0530

    check deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  return(
+    <div className='test'>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem enim aspernatur delectus quibusdam quam temporibus dolore voluptates maiores repellendus doloremque blanditiis nihil minima consequatur, pariatur ad. Illo saepe aliquam molestiae modi ea, beatae error cupiditate similique quia, quis corrupti cumque distinctio voluptates autem unde eaque nostrum delectus eos labore dicta nesciunt inventore? Fuga magnam consequatur aut tenetur ipsa adipisci veritatis?</p>
+    </div>
   )
 }
 
